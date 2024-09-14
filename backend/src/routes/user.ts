7244c71fd5d92115798ba735b24223243213c685
Aryamaan-295 <159464530+Aryamaan-1@users.noreply.github.com>
2024-09-14 20:56:42 +0530

    common

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,6 +2,8 @@ import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
 import { Hono } from 'hono';
 import { sign } from 'hono/jwt';
+import { signupInput } from '@kaviaryamaan/blog-common';
+import { signinInput } from '@kaviaryamaan/blog-common';
 
 
 export const userRouter = new Hono<{
@@ -18,6 +20,14 @@ userRouter.post('/signup', async (c) => {
     }).$extends(withAccelerate())
 
     const body = await c.req.json();
+
+    const { success } = signupInput.safeParse(body);
+    if(!success) {
+        c.status(411);
+        return c.json({
+            error: "Bad Inputs",
+        })
+    }
 
     try {
     const user = await prisma.user.create({
@@ -48,6 +58,15 @@ userRouter.post('/signin', async (c) => {
     }).$extends(withAccelerate());
 
     const body = await c.req.json();
+
+    const { success } = signinInput.safeParse(body);
+    if (!success) {
+        c.status(411);
+        return c.json({
+            error: "Bad Inputs",
+        })
+    }
+
     try {
         const user = await prisma.user.findUnique({
         where: {
