52089e9fecb3f74245084048b174bd5d3eeb3a9d
Aryamaan-295 <159464530+Aryamaan-1@users.noreply.github.com>
2024-09-21 01:57:21 +0530

    Update quote

--- frontend/src/components/Qoute.tsx
+++ frontend/src/components/Qoute.tsx
@@ -2,13 +2,14 @@ export default function Quote() {
     return (
         <div className="bg-gray-300 h-screen flex justify-center flex-col px-32">
             <div className="font-bold text-3xl flex justify-center text-gray-800">
-                "The customer service I received was exceptional. The support team went above and beyond to address my concerns."
+                
+"Your blog is a reflection of who you are. Write with authenticity, and you'll attract readers who resonate with your message."
             </div>
             <div className="mt-4 font-semibold text-lg">
-                Jules Winnfield
+            John Smith
             </div>
             <div className="font-extralight text-gray-600">
-                CEO | Acme Inc
+            Content Creator
             </div>
         </div>
     )
