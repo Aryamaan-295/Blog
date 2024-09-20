37d4f926d497944b677d1d406d5939d7dc991c5b
Aryamaan-295 <159464530+Aryamaan-1@users.noreply.github.com>
2024-09-21 01:40:32 +0530

    route change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -45,7 +45,7 @@ function App() {
         <Routes>
           <Route>
             <Route path="/signup" element={ <Signup/> } />
-            <Route path="/signin" element={ <Signin/> } />
+            <Route path="/" element={ <Signin/> } />
           </Route>
           <Route element={ <ProtectedRoutes />}>
             <Route element={ <MainLayout />}>
